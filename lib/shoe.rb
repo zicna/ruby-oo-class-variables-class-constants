759380d5af2ddc7320219872ec0ab0930cd86673
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-01-01 16:06:45 -0600

    commit tree eab02318f1aad3d774985bf0bdfd0622ce910c3b

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << @brand
+    BRANDS.uniq!
   end
 
   def cobble
@@ -11,4 +15,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  
 end
